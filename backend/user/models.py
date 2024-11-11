7719599322d4064723684e70c3beadbeebe9bd29
Franco Ignacio Villca <ignafrancov@gmail.com>
2024-11-11 00:24:43 -0300

    feat: agregando campos al modelo usuario

--- backend/user/models.py
+++ backend/user/models.py
@@ -6,3 +6,5 @@ class Users(models.Model):
     last_name = models.CharField(max_length=30, blank=False, null=False)
     email = models.CharField(max_length=100, blank=False, null=False)
     phone = models.CharField(max_length=16, blank=False, null=False)
+    address = models.CharField(max_length=100, blank=False, null=True)
+    is_active = models.BooleanField(default=True, blank=True)
--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -4,5 +4,5 @@ from .models import Users
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ('id', 'name', 'last_name', 'email', 'phone')
+        fields = ('id', 'name', 'last_name', 'email', 'phone', 'address', 'is_active')
         read_only_field = 'id'
